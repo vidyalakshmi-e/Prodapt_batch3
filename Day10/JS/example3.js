5eea61742e1a606e06bfcddcc252c6763a9cd61b
vidyalakshmi-e <vidyalakshmi.e@prodapt.com>
2026-07-17 17:42:37 +1300

    Update example3.js

--- Day10/JS/example3.js
+++ Day10/JS/example3.js
@@ -1,6 +1,8 @@
 let marks=[85,42,76,91,38,67,55,29,80,49];
 var t_pass=0;
 var t_fail=0;
+var high=marks[0];
+var low=marks[0];
 
 for (var i=0;i<10;i++){
     if(marks[i] >=50){
@@ -11,6 +13,14 @@ for (var i=0;i<10;i++){
         t_fail++;
         console.log("Student "+i+": "+marks[i]+" - Fail")
     }
+    if(marks[i]>high){
+        high=marks[i];
+    }
+    if(marks[i]<low){
+        low=marks[i];
+    }
 }
 console.log("Total Passed: "+t_pass);
 console.log("Total Failed: "+t_fail);
+console.log("Highest Score: "+high);
+console.log("Lowest Score: "+low);
